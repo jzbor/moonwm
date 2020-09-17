d9e3124efdb3d895ca52a969251a77037cfaa3e2
jzbor <jzbor@posteo.net>
2020-09-17 20:27:13 +0200

    Applying centered window name patch (dwm-centeredwindowname-20180909-6.2.diff)

--- dwm.c
+++ dwm.c
@@ -730,8 +730,9 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - sw - x) > bh) {
 		if (m->sel) {
+			int mid = (m->ww - TEXTW(m->sel->name)) / 2 - x;
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
