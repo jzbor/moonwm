13ffd37f89889c062b724ba6788a13a32412333a
jzbor <jzbor@posteo.net>
2020-10-01 21:13:37 +0200

    Adding thunderbird to ws list

--- config.h
+++ config.h
@@ -48,7 +48,8 @@ static const Rule rules[] = {
 	{ "polybar", NULL,     NULL,           0,         0,          0,           1,        -1 },
 	{ "XClock",  NULL,     NULL,           0,         1,          0,           1,        -1 },
 	{ "Alacritty", NULL,   NULL,           0,         0,          1,           1,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
+	{ "Thunderbird", NULL, NULL,           1 << 8,    0,          0,          -1,        -1 },
 };
 
 /* layout(s) */
