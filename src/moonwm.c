1d0490386da964d7c2118b4ecbe814d5d08958f8
jzbor <jzbor@posteo.net>
2021-09-17 00:19:45 +0200

    Initially adding movedir (placedir) patch

--- src/moonwm.c
+++ src/moonwm.c
@@ -162,6 +162,7 @@ static void moveyfloating(const Arg *arg);
 static void initclientpos(Client *c);
 static Client *nexttagged(Client *c);
 static Client *nexttiled(Client *c);
+static void movedir(const Arg *arg);
 static void placemouse(const Arg *arg);
 static void pop(Client *);
 static void propertynotify(XEvent *e);
@@ -2035,6 +2036,11 @@ void
 movex(const Arg *arg) {
 	if (ISFLOATING(selmon->sel)) {
 		movexfloating(arg);
+	} else if (usemovedir) {
+		if (arg->i < 0)
+			movedir(&((Arg) { .i = 0 }));
+		else
+			movedir(&((Arg) { .i = 1 }));
 	} else {
 		zoom(NULL);
 	}
@@ -2045,10 +2051,16 @@ movey(const Arg *arg) {
 	if (ISFLOATING(selmon->sel)) {
 		arg = &(Arg) { .i = -arg->i };
 		moveyfloating(arg);
-	} else if (arg->i < 0) {
-		pushstack(&((Arg) { .i = INC(+1) }));
+	} else if (usemovedir) {
+		if (arg->i < 0)
+			movedir(&((Arg) { .i = 2 }));
+		else
+			movedir(&((Arg) { .i = 3 }));
 	} else {
-		pushstack(&((Arg) { .i = INC(-1) }));
+		if (arg->i < 0)
+				pushstack(&((Arg) { .i = INC(+1) }));
+		else
+			pushstack(&((Arg) { .i = INC(-1) }));
 	}
 }
 
@@ -2148,6 +2160,103 @@ nexttiled(Client *c)
 {
 	for (; c && (CMASKGET(c, M_FLOATING)  || !ISVISIBLE(c)); c = c->next);
 	return c;
+}
+
+void
+movedir(const Arg *arg)
+{
+	Client *s = selmon->sel, *f = NULL, *c, *next, *fprior, *sprior;
+
+	if (!s || CMASKGET(s, M_FLOATING))
+		return;
+
+	unsigned int score = -1;
+	unsigned int client_score;
+	int dist;
+	int dirweight = 20;
+
+	next = s->next;
+	if (!next)
+		next = s->mon->clients;
+	for (c = next; c != s; c = next) {
+
+		next = c->next;
+		if (!next)
+			next = s->mon->clients;
+
+		if (!ISVISIBLE(c)) // || HIDDEN(c)
+			continue;
+
+		switch (arg->i) {
+		case 0: // left
+			dist = s->x - c->x - c->w;
+			client_score =
+				dirweight * MIN(abs(dist), abs(dist + s->mon->ww)) +
+				abs(s->y - c->y);
+			break;
+		case 1: // right
+			dist = c->x - s->x - s->w;
+			client_score =
+				dirweight * MIN(abs(dist), abs(dist + s->mon->ww)) +
+				abs(c->y - s->y);
+			break;
+		case 2: // up
+			dist = s->y - c->y - c->h;
+			client_score =
+				dirweight * MIN(abs(dist), abs(dist + s->mon->wh)) +
+				abs(s->x - c->x);
+			break;
+		default:
+		case 3: // down
+			dist = c->y - s->y - s->h;
+			client_score =
+				dirweight * MIN(abs(dist), abs(dist + s->mon->wh)) +
+				abs(c->x - s->x);
+			break;
+		}
+
+		if (((arg->i == 0 || arg->i == 2) && client_score <= score) || client_score < score) {
+			score = client_score;
+			f = c;
+		}
+	}
+
+	if (f && f != s) {
+		for (fprior = f->mon->clients; fprior && fprior->next != f; fprior = fprior->next);
+		for (sprior = s->mon->clients; sprior && sprior->next != s; sprior = sprior->next);
+
+		if (s == fprior) {
+			next = f->next;
+			if (sprior)
+				sprior->next = f;
+			else
+				f->mon->clients = f;
+			f->next = s;
+			s->next = next;
+		} else if (f == sprior) {
+			next = s->next;
+			if (fprior)
+				fprior->next = s;
+			else
+				s->mon->clients = s;
+			s->next = f;
+			f->next = next;
+		} else { // clients are not adjacent to each other
+			next = f->next;
+			f->next = s->next;
+			s->next = next;
+			if (fprior)
+				fprior->next = s;
+			else
+				s->mon->clients = s;
+			if (sprior)
+				sprior->next = f;
+			else
+				f->mon->clients = f;
+		}
+
+		arrange(f->mon);
+	}
 }
 
 void
