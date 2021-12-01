28427c775efc4a2baa05487dd1f25f1ddc5356c0
jzbor <jzbor@posteo.net>
2021-12-01 19:33:47 +0100

    Fixing status command

--- src/config.h
+++ src/config.h
@@ -162,9 +162,9 @@ static const char *layoutmenu_cmd = "moonwm-menu layouts-classic";
 
 /* this script or program gets called for bar button presses
  * the button is exported as BUTTON the according module as STATUSCMDN (a number)*/
-static char *statushandler[] = { "/bin/sh", "-c", "$MOONWM_STATUSCMD action", NULL };
+static char *statushandler[] = { "/bin/sh", "-c", "$STATUSCMD action", NULL };
 // this command gets executed on startup and should set you status to WM_NAME
-static char *statuscmd[] = { "/bin/sh", "-c", "$MOONWM_STATUSCMD loop", NULL };
+static char *statuscmd[] = { "/bin/sh", "-c", "$STATUSCMD", NULL };
 
 /* most keys are specified in <X11/keysymdef.h> */
 static Key keys[] = {
